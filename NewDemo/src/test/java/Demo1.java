6936da2499ede50705b7fbcef8a3ba4ef681b6a3
Neelam <Neelam@scom553-pc.sankalp.com>
2021-03-19 11:26:24 +0530

    fdfdfdfd

---
+++ NewDemo/src/test/java/Demo1.java
@@ -0,0 +1,36 @@
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	
+	@Test(priority=0)
+	public void abc()
+	{
+		System.out.println("Test1");
+	}
+	
+	@Test
+	public void xyz()
+	{
+		System.out.println("Test2");
+	}
+	
+	@Test
+	public void acb()
+	{
+		System.out.println("Test3");
+	}
+	
+	@Test
+	public void Test4()
+	{
+		System.out.println("Test4");
+	}
+	
+	@Test
+	public void Test5()
+	{
+		System.out.println("Test5");
+	}
+
+}
